e6290c8e2c127c16851ccca72868df94beed5159
Solomon <sjagyabeng001@st.ug.edu.gh>
2025-09-08 01:55:19 -1200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Ar9jHLCD#U114HmsdIwZMn9VNhW4BV4WzOtpzh-Y2ioNCMBF8aYA",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "0534163468", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
-  TIMEZONE: "Africa/Harare" //put your country timeZone....leave blank if u don't know.
+  TIMEZONE: "Africa/Ghana" //put your country timeZone....leave blank if u don't know.
 };
